aaa3885e0284467c174587c644719bf5445f35d7
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-09-29 18:33:47 +0900

    dd

--- enterance_app/enterance-app/routes.js
+++ enterance_app/enterance-app/routes.js
@@ -1,35 +1,45 @@
 //SPDX-License-Identifier: Apache-2.0
 
 var enterance = require('./controller.js');
-var express = require('express');
-var app2=express();
-var bodyParser = require('body-parser');
+
 
 module.exports = function (app) {
   var GPIO = require('onoff').Gpio,
   led = new GPIO(24, 'out');
 
-
+  
   app.get('/get_enterance/:id', function (req, res) { //GET 메소드 / 주소의 요청일때만 실행된다.
     enterance.get_enterance(req, res);
   });
+
   app.get('/add_barcode/:enterance', function (req, res) {
+    var state = req.body.led;
+    if (state == 'Create') {
+      led.writeSync(1);
+      console.log("on");
+    }
+    else {
+      led.writeSync(0);
+    }
+    // res.sendfile('./client/index.html', { root: __dirname });
+
     enterance.add_barcode(req, res);
   });
+
   app.get('/get_all_enterance', function (req, res) {
     enterance.get_all_enterance(req, res);
   });
+
   app.get('/update_enterance/:updated_timestamp', function (req, res) {
     enterance.update_enterance(req, res);
   });
+}
 
- 
-  app2.use(bodyParser.json());
-  app2.use(bodyParser.urlencoded({ extended: false }));
-  app2.get('/led', function (req, res) {
+
+/*   app.get('/led', function (req, res) {
     res.sendfile('./client/index.html', { root: __dirname });
   });
-  app2.post('/data', function (req, res) {
+  app.post('/data', function (req, res) {
     var state = req.body.led;
     if (state == 'Create') {
       led.writeSync(1);
@@ -40,5 +50,5 @@ module.exports = function (app) {
     }
     res.sendfile('./client/index.html', { root: __dirname });
   });
-}
+} */
 
