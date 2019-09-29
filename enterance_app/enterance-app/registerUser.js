73c49773efb9c46b9133f7d5331c1b93a164bd59
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-09-29 17:18:03 +0900

    sdf

--- enterance_app/enterance-app/registerUser.js
+++ enterance_app/enterance-app/registerUser.js
@@ -55,16 +55,16 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 
     // at this point we should have the admin user
     // first need to register the user with the CA server
-    return fabric_ca_client.register({enrollmentID: 'user3', affiliation: 'org1.department1'}, admin_user);
+    return fabric_ca_client.register({enrollmentID: 'user4', affiliation: 'org1.department1'}, admin_user);
 }).then((secret) => {
     // next we need to enroll the user with CA server
-    console.log('Successfully registered user3 - secret:'+ secret);
+    console.log('Successfully registered user4 - secret:'+ secret);
 
-    return fabric_ca_client.enroll({enrollmentID: 'user3', enrollmentSecret: secret});
+    return fabric_ca_client.enroll({enrollmentID: 'user4', enrollmentSecret: secret});
 }).then((enrollment) => {
-  console.log('Successfully enrolled member user "user3" ');
+  console.log('Successfully enrolled member user "user4" ');
   return fabric_client.createUser(
-     {username: 'user3',
+     {username: 'user4',
      mspid: 'Org1MSP',
      cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
      });
@@ -73,7 +73,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 
      return fabric_client.setUserContext(member_user);
 }).then(()=>{
-     console.log('user3 was successfully registered and enrolled and is ready to intreact with the fabric network');
+     console.log('user4 was successfully registered and enrolled and is ready to intreact with the fabric network');
 
 }).catch((err) => {
     console.error('Failed to register: ' + err);
