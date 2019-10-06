5bcfac9298d005d447b2d00ba8a9303a5dcd3071
jiwon <traveloving2030@gmail.com>
2019-10-06 11:39:20 +0000

    Desktop udpate

---
+++ enterance_app/enterance-app/routesDesktop.js
@@ -0,0 +1,22 @@
+var enterance = require('./controller.js');
+
+
+module.exports = function (app) {
+
+  app.get('/get_enterance/:id', function (req, res) { //GET 메소드 / 주소의 요청일때만 실행된다.
+    enterance.get_enterance(req, res);
+  });
+
+  app.get('/add_barcode/:enterance', function (req, res) {
+    enterance.add_barcode(req, res);
+  });
+
+  app.get('/get_all_enterance', function (req, res) {
+    enterance.get_all_enterance(req, res);
+  });
+
+  app.get('/update_enterance/:updated_timestamp', function (req, res) {
+    var array = req.params.enterance.split("-");
+    enterance.update_enterance(req, res);
+  });
+}
