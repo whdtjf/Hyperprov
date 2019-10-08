3a13a039edc682b32f3c0551382d0d132a81a1d1
KimYC1223 <kau_esc@naver.com>
2019-10-08 20:00:18 +0900

    login function test

---
+++ enterance_app/enterance-app-Desktop/client/fakeApp.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var app = angular.module('fakeApplication', []);
+let allQueryData =[];
+let testStr = "hi!!";
+
+
+app.controller ('fakeAppController', ['$scope',($scope) => {
+
+  allQueryData = [ {key:2013122041, name:"김영찬", timestamp:Date.now()},
+                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000)},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*2)},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*3)},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*4)},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*5)},
+                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000*6)},
+                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000*7)},
+                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*8)},
+                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*9)},
+                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*10)},
+                         ];
+
+  $scope.all_enterance = allQueryData;
+}])
--- enterance_app/enterance-app-Desktop/routes.js
+++ enterance_app/enterance-app-Desktop/routes.js
@@ -1,7 +1,22 @@
 var enterance = require('./controller.js');
-
+var sessionParser = require('express-session');
 
 module.exports = function (app) {
+<<<<<<< HEAD
+  app.use(sessionParser({
+    secret: 'hypershield',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+출처: https://zzdd1558.tistory.com/178 [신입 개발자]
+
+  app.post('/',(req,res)=>{
+      res.sendFile("hello!");
+  });
+=======
+>>>>>>> 1f2da1ec93eb846b26fcda090e5d27de2d00575a
 
   app.get('/get_enterance/:id', function (req, res) {
     //GET 메소드 / 주소의 요청일때만 실행된다.
