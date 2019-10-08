c104b8fab817c0fc1536b4f086faefae349ba570
KimYC1223 <kau_esc@naver.com>
2019-10-09 04:03:15 +0900

    session save/load test 5

--- enterance_app/enterance-app-Desktop/client/fakeApp.js
+++ enterance_app/enterance-app-Desktop/client/fakeApp.js
@@ -25,7 +25,7 @@ app.controller ('fakeAppController', ['$scope',($scope) => {
   $scope.setIDtoSession = (uID) => {
     try {
       sessionStorage.setItem("uID", JSON.stringify(uID));
-      alert("storage : "sessionStorage.getItem("uID"));
+      alert("storage : "+sessionStorage.getItem("uID"));
     } catch (e) {
       alert("세션 저장중 에러..!\n"+e);
     }
