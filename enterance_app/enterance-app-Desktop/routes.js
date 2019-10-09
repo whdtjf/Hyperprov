d5b89a28d528f29771dd629130baae87fa1afdd5
KimYC1223 <kau_esc@naver.com>
2019-10-09 22:35:21 +0900

    ng-repeat test 18

--- enterance_app/enterance-app-Desktop/routes.js
+++ enterance_app/enterance-app-Desktop/routes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 
   app.set('view engine','ejs');
   app.get('/mainPage', (req,res) => {
-    res.render(__dirname+'/client/mainPage.ejs',{uID:sessionStorage.getItem('uID')});
+    res.render(__dirname+'/client/mainPage.ejs');
   });
 
   app.get('/get_enterance/:id', function (req, res) {
