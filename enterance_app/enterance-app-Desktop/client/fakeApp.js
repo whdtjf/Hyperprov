572ed49c6e492551b686cf4c88a57c6bb8b83b74
KimYC1223 <kau_esc@naver.com>
2019-10-09 19:03:03 +0900

    merging main page

--- enterance_app/enterance-app-Desktop/client/fakeApp.js
+++ enterance_app/enterance-app-Desktop/client/fakeApp.js
@@ -6,16 +6,16 @@ let allQueryData =[];
 app.controller ('fakeAppController', ['$scope',($scope) => {
 
   allQueryData = [ {key:2013122041, name:"김영찬", timestamp:Date.now()},
-                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000)},
-                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*2)},
-                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*3)},
-                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*4)},
-                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*5)},
-                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000*6)},
-                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000*7)},
-                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*8)},
-                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*9)},
-                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*10)},
+                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000),location:"gate_A",state:"in"},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*2),location:"gate_B",state:"in"},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*3),location:"gate_B",state:"out"},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*4),location:"gate_A",state:"in"},
+                           {key:2013122201, name:"이승준", timestamp:(Date.now()+1000*5),location:"gate_A",state:"out"},
+                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000*6),location:"gate_B",state:"in"},
+                           {key:2013122041, name:"김영찬", timestamp:(Date.now()+1000*7),location:"gate_A",state:"out"},
+                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*8),location:"gate_A",state:"in"},
+                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*9),location:"gate_B",state:"out"},
+                           {key:2013122260, name:"정지원", timestamp:(Date.now()+1000*10),location:"gate_B",state:"in"},
                          ];
 
   $scope.all_enterance = allQueryData;
