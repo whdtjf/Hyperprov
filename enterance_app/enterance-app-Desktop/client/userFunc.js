fea8b6cb5ceee4c3a85e747d476e2e96070e1046
KimYC1223 <kau_esc@naver.com>
2019-10-14 06:05:05 +0900

    google chart update 10

--- enterance_app/enterance-app-Desktop/client/userFunc.js
+++ enterance_app/enterance-app-Desktop/client/userFunc.js
@@ -4,6 +4,10 @@ let currentTime = new Date();
 let timeStr = '2019-10-10';
 console.log('currentTime : '+timeStr);
 
+
+google.charts.setOnLoadCallback(drawEnterChart);
+google.charts.setOnLoadCallback(drawExitChart);
+
 let rawDataEnter = [['gate_A',0],['gate_B',0]];
 let rawDataExit = [['gate_A',0],['gate_B',0]];
 
@@ -61,61 +65,3 @@ function drawExitChart() {
   let exitChart = new google.visualization.PieChart(document.getElementById('chart-exit'));
   exitChart.draw(data,options);
 }
-
-google.charts.setOnLoadCallback(drawEnterChart);
-google.charts.setOnLoadCallback(drawExitChart);
-
-
-
-
-setTimeout ( () => {
-  let allEntranceLog = queryAllEntrance();
-
-  for (let i = 0 ; i < allEntranceLog.length ; i ++) {
-      if (allEntranceLog[i].timestamp.startWith(currentTime)){
-        let flag = false;  let index = 0;
-
-        //------------------------------------------------------------------------
-        // 입장한 경우
-        //------------------------------------------------------------------------
-        if (allEntranceLog[i].state == 'in'){
-          for(let j = 0; j < rawDataEnter.length; j++) {
-            if (allEntranceLog[i].location == rawDataEnter[j][0]){
-              flag = true;
-              index = j;
-              break;
-            }
-          }
-
-          if(flag) {
-            let count = rawDataEnter[index][1] + 1;
-            rawDataEnter.splice(index, 1);
-            rawDataEnter.splice(index,0,[allEntranceLog[i].location,count]);
-          } else {
-            rawDataEnter.push([allEntranceLog[i].location,1]);
-          }
-        //------------------------------------------------------------------------
-        // 퇴장한 경우
-        //------------------------------------------------------------------------
-        } else {
-          for(let j = 0; j < rawDataExit.length; j++) {
-            if (allEntranceLog[i].location == rawDataExit[j][0]){
-              flag = true;
-              index = j;
-              break;
-            }
-          }
-
-          if(flag) {
-            let count = rawDataExit[index][1] + 1;
-            rawDataExit.splice(index, 1);
-            rawDataExit.splice(index,0,[allEntranceLog[i].location,count]);
-          } else {
-            rawDataExit.push([allEntranceLog[i].location,1]);
-          }
-        }
-      }
-    }
-    drawEnterChart();
-    drawExitChart();
-  },1000);
