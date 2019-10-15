ad55133a5f86e8cfd887ce7441ce0f45b85eeb7d
KimYC1223 <kau_esc@naver.com>
2019-10-15 18:56:25 +0900

    remove sort func (queryAllHistory in fake.app)

--- enterance_app/enterance-app-Desktop/client/fakeApp.js
+++ enterance_app/enterance-app-Desktop/client/fakeApp.js
@@ -18,7 +18,7 @@ let queryAllHistory = () => {
       history.add(temp[j]);
     }
   }
-  history.sort( (str1,str2) => {  return ( ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 ) ); });
+  //history.sort( (str1,str2) => {  return ( ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 ) ); });
 
   return history;
 }
