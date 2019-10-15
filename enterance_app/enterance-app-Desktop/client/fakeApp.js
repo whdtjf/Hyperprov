95aa739fc325c27e17634fb17b1d161726504c4c
KimYC1223 <kau_esc@naver.com>
2019-10-15 19:29:59 +0900

    update queryAllHistory Func

--- enterance_app/enterance-app-Desktop/client/fakeApp.js
+++ enterance_app/enterance-app-Desktop/client/fakeApp.js
@@ -12,14 +12,14 @@ let queryAllEntrance;
 let queryEnterance;
 let queryAllHistory = () => {
   let history = [];
-  for( let i = 0 ; i < queryAllEntrance.length ; i++ ){
-    let temp = queryHistory(queryAllEntrance[i].name);
+  let allEntrance = queryAllEntrance();
+  for( let i = 0 ; i < allEntrance.length ; i++ ){
+    let temp = queryHistory(allEntrance[i].name);
     for (let j = 0 ; j < temp.length ; j ++ ){
       history.add(temp[j]);
     }
   }
-  //history.sort( (str1,str2) => {  return ( ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 ) ); });
-
+  history.sort( (str1,str2) => {  return ( ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 ) ); });
   return history;
 }
 
