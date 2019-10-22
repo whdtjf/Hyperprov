53de451fac773276a0bb4904ba912311a19f44cd
jiwon <traveloving2030@gmail.com>
2019-10-22 15:15:25 +0900

    Update registerAdmin.js

--- enterance_app/enterance-app/registerAdmin.js
+++ enterance_app/enterance-app/registerAdmin.js
@@ -18,9 +18,14 @@ async function main() {
 
         // Create a new CA client for interacting with the CA.
         const caInfo = ccp.certificateAuthorities['ca.ptunstad.no'];
+        console.log(caInfo);
+        console.log("1st ok");
         const caTLSCACerts = caInfo.tlsCACerts.pem;
+        console.log(caTLSCACerts);
+        console.log("2nd ok");
         const ca = new FabricCAServices(caInfo.url, { trustedRoots: caTLSCACerts, verify: false }, caInfo.caName);
-
+        console.log(ca);
+        console.log("3rd ok");
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
         const wallet = new FileSystemWallet(walletPath);
@@ -28,6 +33,8 @@ async function main() {
 
         // Check to see if we've already enrolled the admin user.
         const adminExists = await wallet.exists('admin');
+    
+
         if (adminExists) {
             console.log('An identity for the admin user "admin" already exists in the wallet');
             return;
@@ -35,6 +42,7 @@ async function main() {
 
         // Enroll the admin user, and import the new identity into the wallet.
         const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+      
         const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
         await wallet.import('admin', identity);
         console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
