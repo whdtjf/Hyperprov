0c2b636d107dbd85fb3547e5d894a4fd05f96022
jiwon <traveloving2030@gmail.com>
2019-10-22 23:38:59 +0900

    Update controller.js

--- enterance_app/enterance-app/controller.js
+++ enterance_app/enterance-app/controller.js
@@ -78,14 +78,17 @@ module.exports = (function () {
 
 					// Get the network (channel) our contract is deployed to.
 					const network = await gateway.getNetwork('mychannel');
-
+					console.log(network);
+					console.log("1st");
 					// Get the contract from the network.
 					const contract = network.getContract('Enterance_Code');
-
+					console.log(contract);
+					console.log("2nd");
 					// Evaluate the specified transaction.
 					// queryEnterance transaction - requires 1 argument, ex: ('queryEnterance', '0101092')
 					// queryAllEnterance transaction - requires no arguments, ex: ('queryAllEnterance')
 					const query_responses = await contract.evaluateTransaction('queryAllEnterance');
+					
 					console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
 					if (query_responses && query_responses.length == 1) {
