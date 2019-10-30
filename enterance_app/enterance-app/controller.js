aad2c79362cb849b2172e26f7e0b67def3c33fcb
jiwon <traveloving2030@gmail.com>
2019-10-31 01:35:17 +0900

    Update controller.js

--- enterance_app/enterance-app/controller.js
+++ enterance_app/enterance-app/controller.js
@@ -20,7 +20,7 @@ var os = require('os');
 
 const { FileSystemWallet, Gateway, X509WalletMixin } = require('fabric-network');
 const ccpPath = path.resolve(__dirname, '..', '..', 'scripts', 'connection-org1.json');
-var manager = fs.readFileSync('Barcode.txt');
+var manager = fs.readFileSync('Barcode.txt').toString();
 console.log(manager);
 
 
