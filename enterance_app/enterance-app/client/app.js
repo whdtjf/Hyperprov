ef75a00989512fe1759f8354df4a8f42898c5cd5
whdtjf <45926066+traveloving2030@users.noreply.github.com>
2019-11-02 16:08:08 +0900

    1

--- enterance_app/enterance-app/client/app.js
+++ enterance_app/enterance-app/client/app.js
@@ -31,79 +31,90 @@ let monthlyLogGateB = [];
 // Angular Controller
 app.controller('appController', function($scope, appFactory){
 
-	$("#success_holder").hide();
-	$("#success_create").hide();
-	$("#error_holder").hide();
-	$("#error_query").hide();
+   $("#success_holder").hide();
+   $("#success_create").hide();
+   $("#error_holder").hide();
+   $("#error_query").hide();
 
-	//queryAllenterance 라는 ng-click에 function() 이하를 넣는다
-	$scope.queryAllEnterance = function(){
-		appFactory.queryAllEnterance(function(data){ //appFactory.queryAllEnterance하면 get방식으로 enterance모든 데이터가 callback으로 넘겨짐
-			var array = [];
-			for (var i = 0; i < data.length; i++){
-				parseInt(data[i].Key);
-				data[i].Record.Key = parseInt(data[i].Key);
-				array.push(data[i].Record);
-			}
-			array.sort(function(a, b) {
-			    return parseFloat(a.Key) - parseFloat(b.Key);
-			});
-			allStatusData = array;
-			return allStatusData;
-		});
-	}
-	queryAllEntrance = $scope.queryAllEnterance;
-
-	$scope.queryEnterance = function(Barcode){
-		appFactory.queryEnterance(Barcode, function(data){
-			if (data == "Could not locate enterance"){
-				$("#error_query").show();
-			} else{
-				$("#error_query").hide();
-			}
-
-			return data;
-		});
-	}
-	queryEnterance = $scope.queryEnterance
-
-	$scope.queryHistory = function(Barcode){
-		appFactory.queryEnterance(Barcode, function(data){
-			if (data == "Could not locate enterance"){
-				$("#error_query").show();
-			} else{
-				$("#error_query").hide();
-			}
-			return data;
-		});
-	}
-	queryHistory = $scope.queryHistory
-
-	$scope.recordBarcode = function(){
-
-		appFactory.recordBarcode($scope.enterance, function(data){
-			$scope.create_barcode = data;
-			$("#success_create").show();
-		});
-	}
-
-	$scope.UpdateEnterance = function(){
-
-		appFactory.UpdateEnterance($scope.newEnterance, function(data){
-			$scope.update_timestamp = data;
-			if ($scope.update_timestamp == "Error: no enterance catch found"){
-				$("#error_holder").show();
-				$("#success_holder").hide();
-			} else{
-				$("#success_holder").show();
-				$("#error_holder").hide();
-			}
-		});
-	}
+   //queryAllenterance 라는 ng-click에 function() 이하를 넣는다
+   $scope.queryAllEnterance = function(){
+      appFactory.queryAllEnterance(function(data){ //appFactory.queryAllEnterance하면 get방식으로 enterance모든 데이터가 callback으로 넘겨짐
+         var array = [];
+         for (var i = 0; i < data.length; i++){
+            parseInt(data[i].Key);
+            data[i].Record.Key = parseInt(data[i].Key);
+            array.push(data[i].Record);
+         }
+         array.sort(function(a, b) {
+             return parseFloat(a.Key) - parseFloat(b.Key);
+         });
+         allStatusData = array;
+         return allStatusData;
+      });
+   }
+   queryAllEntrance = $scope.queryAllEnterance;
 
 
+  
+  $scope.queryEnterance = function(){
 
-	//=================================================================
+      var id = $scope.enterance_id;
+
+      appFactory.queryEnterance(id, function(data){
+         $scope.query_enterance = data;
+
+         if ($scope.query_enterance == "Could not locate enterance"){
+            console.log()
+            $("#error_query").show();
+         } else{
+            $("#error_query").hide();
+      }
+      return data;
+      });
+   }
+   queryEnterance = $scope.queryEnterance
+
+  $scope.queryHistory = function(){
+
+      var id = $scope.enterance_id;
+
+      appFactory.queryHistory(id, function(data){
+         $scope.query_history = data;
+
+         if ($scope.query_history == "Could not locate enterance"){
+            console.log()
+            $("#error_query").show();
+         } else{
+            $("#error_query").hide();
+      }
+      return data;
+      });
+   }
+   queryHistory = $scope.queryHistory
+
+   $scope.recordBarcode = function(){
+
+      appFactory.recordBarcode($scope.enterance, function(data){
+         $scope.create_barcode = data;
+         $("#success_create").show();
+      });
+   }
+
+   $scope.UpdateEnterance = function(){
+
+      appFactory.UpdateEnterance($scope.newEnterance, function(data){
+         $scope.update_timestamp = data;
+         if ($scope.update_timestamp == "Error: no enterance catch found"){
+            $("#error_holder").show();
+            $("#success_holder").hide();
+         } else{
+            $("#success_holder").show();
+            $("#error_holder").hide();
+         }
+      });
+   }
+
+   //=================================================================
   //  ADD REAL app.js
   //=================================================================
   $scope.queryHistoryTop10 = () => {
@@ -262,50 +273,53 @@ app.controller('appController', function($scope, appFactory){
 // Angular Factory
 app.factory('appFactory', function($http){
 
-	var factory = {};
+   var factory = {};
 
     factory.queryAllEnterance = function(callback){
 
-    	$http.get('/get_all_enterance/').success(function(output){
-			callback(output)
-		});
-	}
+       $http.get('/get_all_enterance/').success(function(output){
+         callback(output)
+      });
+   }
 
-	factory.queryEnterance = function(id, callback){
-		$http.get('/get_enterance/'+id)
-		.then(function success(output){
-			console.log(output);
-			callback(output)
-		}, function error(err){
-			console.error(err);
-			callback(err);
-		});
-	}
+   factory.queryEnterance = function(id, callback){
+      $http.get('/get_enterance/'+id)
+      .then(function success(output){
+         console.log(output);
+         callback(output)
+      }, function error(err){
+         console.error(err);
+         callback(err);
+      });
+   }
 
-	factory.queryHistory = (id, callback) => {
-		$http.get('/get_history/'+id)
-		.then(function success(output){
-			callback(output)
-		})
-	}
+   factory.queryHistory = (id, callback) => {
+      $http.get('/get_history/'+id)
+      .then(function success(output){
+         callback(output)
+      }, function error(err){
+         console.error(err);
+         callback(err);
+      });
+   }
 
-	factory.recordBarcode = function(data, callback){
+   factory.recordBarcode = function(data, callback){
 
-		var enterance = data.id + "-" + data.name + "-" + data.timestamp+ "-" + data.location+ "-" + data.state;
+      var enterance = data.id + "-" + data.name + "-" + data.timestamp+ "-" + data.location+ "-" + data.state;
 
-    	$http.get('/add_barcode/'+enterance).success(function(output){
-			callback(output)
-		});
-	}
+       $http.get('/add_barcode/'+enterance).success(function(output){
+         callback(output)
+      });
+   }
 
-	factory.UpdateEnterance = function(data, callback){
+   factory.UpdateEnterance = function(data, callback){
 
-		var updated_enterance = data.id + "-" + data.timestamp+ "-" + data.location+ "-" + data.state;
+      var updated_enterance = data.id + "-" + data.timestamp+ "-" + data.location+ "-" + data.state;
 
-    	$http.get('/update_enterance/'+updated_enterance).success(function(output){
-			callback(output)
-		});
-	}
+       $http.get('/update_enterance/'+updated_enterance).success(function(output){
+         callback(output)
+      });
+   }
 
-	return factory;
+   return factory;
 });
