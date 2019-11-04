df182b92c2885994a3841a56ae7e72782eb5f415
KimYC1223 <kau_esc@naver.com>
2019-11-04 23:27:19 +0900

    test

---
+++ enterance_app/enterance-app-Desktop/client/mainPageApp.js
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict';
+
+var app = angular.module('application', []);
+
+//=================================================================
+//  ADD REAL app.js
+//=================================================================
+let allHistoryData = [];
+let allStatusData = [];
+let queryHistory;
+let queryHistory_result;
+let queryAllEntrance;
+let queryAllEntrance_result;
+let queryEnterance;
+let queryEnterance_result;
+
+let id_temp;
+let queryAllHistory_result;
+let queryAllHistory = () => {
+  let history = [];
+  let allEntrance = queryAllEntrance();
+  for( let i = 0 ; i < allEntrance.length ; i++ ){
+    let temp = queryHistory(allEntrance[i].Key);
+    for (let j = 0 ; j < temp.length ; j ++ ){
+      history.push(temp[j]);
+    }
+  }
+  //history.sort( (a,b) => {  return ( ( a.timestamp == b.timestamp ) ? 0 : ( ( a.timestamp > b.timestamp ) ? 1 : -1 ) ); });
+  queryAllHistory_result = history;
+  return history;
+}
+
+let monthlyLogGateA = [];
+let monthlyLogGateB = [];
+
+
+// Angular Controller
+app.controller('appController', function($scope, appFactory,$filter){
+
+   //queryAllenterance 라는 ng-click에 function() 이하를 넣는다
+   $scope.queryAllEnterance = function(){
+      appFactory.queryAllEnterance(function(data){ //appFactory.queryAllEnterance하면 get방식으로 enterance모든 데이터가 callback으로 넘겨짐
+         var array = [];
+         for (var i = 0; i < data.length; i++){
+            parseInt(data[i].Key);
+            data[i].Record.Key = parseInt(data[i].Key);
+            array.push(data[i].Record);
+         }
+         array.sort(function(a, b) {
+             return parseInt(a.Key) - parseInt(b.Key);
+         });
+         allStatusData = array;
+         return allStatusData;
+      });
+   }
+   queryAllEntrance = $scope.queryAllEnterance;
+
+
+
+  $scope.queryEnterance = function(id){
+      appFactory.queryEnterance(id, function(data){
+        console.log(data);
+        queryEnterance_result = data;
+        return data;
+      });
+   }
+   queryEnterance = $scope.queryEnterance
+   id_temp= $scope.enterance_id; //id확인
+
+   $scope.queryHistory = function(id){
+      appFactory.queryHistory(id, function(data){
+        queryHistory_result = data;
+        return data;
+      });
+   }
+   queryHistory = $scope.queryHistory
+
+   //=================================================================
+  //  ADD REAL app.js
+  //=================================================================
+  $scope.queryHistoryTop10 = () => {
+    queryHistoryTop10_2nd($scope.queryHistory(userData.Key))
+  }
+
+  let queryHistoryTop10_2nd = (data) {
+    let arr =[];
+    data.sort( (a,b) => {  return ( ( a.timestamp == b.timestamp ) ? 0 : ( ( a.timestamp > b.timestamp ) ? -1 : 1 ) ); });
+    for (let i = 0 ;  i < data.length; i ++){
+      arr.push(getArr[i]);
+      if (arr.length == 8) break;
+    }
+    $scope.queryHistoryTop10_result = array;
+  }
+
+  $scope.selectDate = {
+       value: new Date()
+  };
+});
+
+// Angular Factory
+app.factory('appFactory', function($http){
+
+   var factory = {};
+
+    factory.queryAllEnterance = function(callback){
+
+       $http.get('/get_all_enterance/').success(function(output){
+         callback(output)
+      });
+   }
+
+   factory.queryEnterance = function(id, callback){
+      $http.get('/get_enterance/'+id)
+      .then(function success(output){
+         console.log(output);
+         callback(output)
+      }, function error(err){
+         console.error(err);
+         callback(err);
+      });
+   }
+
+   factory.queryHistory = (id, callback) => {
+      $http.get('/get_history/'+id)
+      .then(function success(output){
+         callback(output)
+      }, function error(err){
+         console.error(err);
+         callback(err);
+      });
+   }
+
+   factory.recordBarcode = function(data, callback){
+
+      var enterance = data.id + "-" + data.name + "-" + data.timestamp+ "-" + data.location+ "-" + data.state;
+
+       $http.get('/add_barcode/'+enterance).success(function(output){
+         callback(output)
+      });
+   }
+
+   factory.UpdateEnterance = function(data, callback){
+
+      var updated_enterance = data.id + "-" + data.timestamp+ "-" + data.location+ "-" + data.state;
+
+       $http.get('/update_enterance/'+updated_enterance).success(function(output){
+         callback(output)
+      });
+   }
+
+   return factory;
+});
