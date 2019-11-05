b974ff3d3afacf9ffa46b35d84f0d0effaf54a22
KimYC1223 <kau_esc@naver.com>
2019-11-05 22:41:18 +0900

    test

--- enterance_app/enterance-app-Desktop/client/mainPageApp.js
+++ enterance_app/enterance-app-Desktop/client/mainPageApp.js
@@ -58,9 +58,10 @@ app.controller('appController', function($scope,$filter,$http) {
         $http.get('/get_history/'+key).then(function success2(rawData2){
           let data2 = rawData2.data
           // 해당 유저의 모든 히스토리 기록을 저장함
-          for (let j = 0; j < rawData2.length; j ++)
+          for (let j = 0; j < rawData2.length; j ++){
             queryAllHistory_result.push(rawData2[j].Value)
             console.log(rawData2[j].Value)
+          }
         })
       }
     })
