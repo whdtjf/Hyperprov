52002d7f4bb208b81b440b317da6da2393817a53
KimYC1223 <kau_esc@naver.com>
2019-11-12 19:03:17 +0900

    test

--- enterance_app/enterance-app-Desktop/client/localIP.js
+++ enterance_app/enterance-app-Desktop/client/localIP.js
@@ -1,3 +1,8 @@
+let localIP
+
+getLocalIPs(function(ips) { // <!-- ips is an array of local IP addresses.
+  localIP = ips.toString()
+});
 
 function getLocalIPs(callback) {
 try {
@@ -19,8 +24,11 @@ pc.onicecandidate = function(e) {
 if (!e.candidate) { // Candidate gathering completed.
 pc.close();
 callback(ips);
-return ;
+return;
 }
+var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+ips.push(ip);
 };
 pc.createOffer(function(sdp) {
 pc.setLocalDescription(sdp);
