0793121e78883a5dc469e7096614f3f64116b480
KimYC1223 <kau_esc@naver.com>
2019-11-12 16:57:50 +0900

    test 2

--- enterance_app/enterance-app-Wallet/server.js
+++ enterance_app/enterance-app-Wallet/server.js
@@ -40,7 +40,7 @@ require('./routes.js')(app);
 app.use(express.static(path.join(__dirname, './client')));
 
 // Save our port
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 4000;
 
 // Start the server and listen on port
 app.listen(port,function(){
