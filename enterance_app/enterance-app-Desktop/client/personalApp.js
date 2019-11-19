14b9c14434ef668acaaffe26356310fad89bb425
KimYC1223 <kau_esc@naver.com>
2019-11-19 22:01:04 +0900

    zzz

--- enterance_app/enterance-app-Desktop/client/personalApp.js
+++ enterance_app/enterance-app-Desktop/client/personalApp.js
@@ -116,7 +116,7 @@ app.controller('appController', function($scope,$filter,$http) {
       $scope.currentPage = page
       for (let i = 0 ; i < 6 ; i ++ ) {
         if ( $scope.currentPage * 6 + i >= $scope.personalLogs.length ) break
-        $scope.showingPersonalLog.push($scope.personalLogs[currentPage * 6 + i])
+        $scope.showingPersonalLog.push($scope.personalLogs[page * 6 + i])
       }
     }
   }
